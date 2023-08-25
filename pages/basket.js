a7f8ed75df89e187e5f7b9f3a52905aa5327b2ff
AnnVinogradova <saikotakaki@gmail.com>
2023-08-25 13:51:04 +0300

    Сохраняет, но сломался + и -

--- pages/basket.js
+++ pages/basket.js
@@ -1,26 +1,29 @@
 import Shop from '../components/Shop/Shop.js'
 import Instagram from '../components/Instagram/Instagram.js';
-import { chakra, Box, Flex, Input, Link, Text, Heading } from '@chakra-ui/react';
+import { chakra, Box, Flex, Input, Link, Text, Heading, Image } from '@chakra-ui/react';
 import {List, ListItem, ListIcon, OrderedList, UnorderedList,} from '@chakra-ui/react'
 import { useContext } from 'react';
 import { CustomContext } from '../context/context.js';
 
 export default function Basket() {
   const { basket } = useContext(CustomContext);
-  
+
   return <>
 <Box>
   <Box>
   <Link href = "/catalog">К выбору букета</Link>
   <Box>
   <Heading mt={30} fontSize={36} color="rgba(3, 69, 59)" mb={30}>Корзина</Heading>
-  <Text>В корзине 2 товара</Text>
+  <Text>В корзине {basket.length} товар(а)</Text>
   </Box>
   <UnorderedList>
     {basket.map((item)=>(
       <ListItem key={item.id}>
+        <Image src={`../${item.image}`} width={"440"} height={287} />
         {item.title}
+        {item.price}
       </ListItem>
+      
     ))}
   
 </UnorderedList>
